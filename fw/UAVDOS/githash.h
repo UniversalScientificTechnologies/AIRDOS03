9a05e2daeeae637de255e3c617a6ea82ceeb09fc
Jakub Kakona <kaklik@mlab.cz>
2025-06-29 16:19:16 +0200

    Add firmware.

---
+++ fw/UAVDOS/githash.h
@@ -0,0 +1,5 @@
+// This file is overwritten by github actions, do not update it manually
+String githash = "982ca03bf1ce16e72879a7862f0255dc6489c66e,User";
+#define GHRELEASE 4
+#define GHBUILD 0
+#define GHBUILDTYPE User
