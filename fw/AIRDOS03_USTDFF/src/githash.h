c72c3f75542eb6f3f554cdbb694b2b54a7d164c6
kaklik <kaklik@users.noreply.github.com>
2026-02-19 23:19:56 +0000

    Cleanup unused routines., extended build

--- fw/AIRDOS03_USTDFF/src/githash.h
+++ fw/AIRDOS03_USTDFF/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a78a207ed6c95d547feba3e7a85395125f56477b,User";
+String githash = "1a42fdef33d580ff67d79f5ff88876706606641d,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
