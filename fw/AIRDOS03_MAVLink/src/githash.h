0ff7ca531f410e3cc59ce743060ff5cbd1211505
kaklik <kaklik@users.noreply.github.com>
2026-02-19 21:13:43 +0000

    Make auto-commit step repository-agnostic (#3), extended build

---
+++ fw/AIRDOS03_MAVLink/src/githash.h
@@ -0,0 +1,5 @@
+// This file is overwritten by github actions, do not update it manually
+String githash = "a78a207ed6c95d547feba3e7a85395125f56477b,User";
+#define GHRELEASE 4
+#define GHBUILD 0
+#define GHBUILDTYPE User
