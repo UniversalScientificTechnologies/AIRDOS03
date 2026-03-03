f66f77e2502ad7f0ec50ae801a12a959124469ed
Roman Dvorak <romandvorak@mlab.cz>
2026-03-03 18:18:52 +0100

    add PIO AIRDOS04 FW

---
+++ fw/AIRDOS03/githash.h
@@ -0,0 +1,5 @@
+// This file is overwritten by github actions, do not update it manually
+String githash = "52776e93b097287554015d8ede6ca62a418bb035,User";
+#define GHRELEASE 0
+#define GHBUILD 0
+#define GHBUILDTYPE User
