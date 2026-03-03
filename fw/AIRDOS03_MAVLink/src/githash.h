e89fca0879b2278e24a77c57492592a31441cf56
roman-dvorak <roman-dvorak@users.noreply.github.com>
2026-03-03 17:31:10 +0000

    Merge branch 'AIRDOS03B' of github.com:UniversalScientificTechnologies/AIRDOS03 into AIRDOS03B # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit., extended build

--- fw/AIRDOS03_MAVLink/src/githash.h
+++ fw/AIRDOS03_MAVLink/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "f66f77e2502ad7f0ec50ae801a12a959124469ed,User";
+String githash = "33f3676f16d5c22faa3ae9070397232c204d599d,User";
 #define GHRELEASE 4
 #define GHBUILD 0
 #define GHBUILDTYPE User
