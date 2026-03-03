56867bf46af196f6b3e5b0bf9784c4a82bb1431e
kaklik <kaklik@users.noreply.github.com>
2026-03-03 13:26:30 +0000

    Remove redundant definitions., extended build

--- fw/AIRDOS03_MAVLink/src/githash.h
+++ fw/AIRDOS03_MAVLink/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "1a42fdef33d580ff67d79f5ff88876706606641d,User";
+String githash = "a4be568396e706e11f2f33a9044455001ce7d422,User";
 #define GHRELEASE 4
 #define GHBUILD 0
 #define GHBUILDTYPE User
