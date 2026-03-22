28ab3960766e72dd8f860faca66b4706dcb5846b
kaklik <kaklik@users.noreply.github.com>
2026-03-22 12:06:44 +0000

    Implement GNSS based timing. Replace wrong SHT4x readout with SHT31 readout code., extended build

--- fw/AIRDOS03_MAVLink/src/githash.h
+++ fw/AIRDOS03_MAVLink/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "7588d376876462067003e46b85fff5c39365f8c9,User";
+String githash = "6958476e17bdaab430287fc5894cb5f11a0c33cf,User";
 #define GHRELEASE 4
 #define GHBUILD 0
 #define GHBUILDTYPE User
