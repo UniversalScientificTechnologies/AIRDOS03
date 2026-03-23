afdb076d7b1ffaa66045c7f6ab2a760cd57242a7
kaklik <kaklik@users.noreply.github.com>
2026-03-23 19:48:40 +0000

    Merge branch 'AIRDOS03B' of github.com:UniversalScientificTechnologies/AIRDOS03 into AIRDOS03B, extended build

--- fw/AIRDOS03_USTDFF/src/githash.h
+++ fw/AIRDOS03_USTDFF/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "6958476e17bdaab430287fc5894cb5f11a0c33cf,User";
+String githash = "9dd6a5f3c0d23d2b8d27a68746a401ac585bbca4,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
