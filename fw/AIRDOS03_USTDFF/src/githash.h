dd1e1490fb8c30a7d62b03c3b41b7fc9b600fe2d
slimonslimon <slimonslimon@users.noreply.github.com>
2026-04-29 09:19:52 +0000

    Feat: 60s alive broadcast in mavlink fw, extended build

--- fw/AIRDOS03_USTDFF/src/githash.h
+++ fw/AIRDOS03_USTDFF/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "8faaabe67dc26f64a22cb9f278034172a4934a0f,User";
+String githash = "440036f2a383782f566d08d80d9271fef7e5c534,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
