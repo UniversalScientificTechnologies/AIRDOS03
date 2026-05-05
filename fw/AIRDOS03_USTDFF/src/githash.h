2d5c054a89ea245cc3ea6b13c8761ab9f75328d2
kaklik <kaklik@users.noreply.github.com>
2026-05-05 10:43:15 +0000

    Add ulog to AIRDOS data format conversion script. Add measured data plotting script., extended build

--- fw/AIRDOS03_USTDFF/src/githash.h
+++ fw/AIRDOS03_USTDFF/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "440036f2a383782f566d08d80d9271fef7e5c534,User";
+String githash = "0bfabe81ee1272851d87bf6e11d03ab27536aef9,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
