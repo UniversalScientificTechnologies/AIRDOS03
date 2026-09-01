96db8cfe3089bebbb17ff81e0ef4865171c5a0cb
ustbot <ust-bot@seznam.cz>
2026-09-01 17:37:29 +0000

    Merge pull request #14 from UniversalScientificTechnologies/update_release, extended build, extended build, extended build

--- fw/AIRDOS03_MAVLink/src/githash.h
+++ fw/AIRDOS03_MAVLink/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "c9ec31f9b48d9b99c2f7d4d02e0ec3f3199f139e,User";
-#define GHRELEASE 0
+String githash = "c899d9e827d6bcb9975bc992f2cd0a8922199e11,User";
+#define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
