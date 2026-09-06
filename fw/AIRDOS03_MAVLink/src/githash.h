cebab8a80c5def1e2802c7ac991740b64ebb6fb7
ustbot <ust-bot@seznam.cz>
2026-09-06 17:16:33 +0000

    Merge pull request #16 from UniversalScientificTechnologies/feature/event-buffer-overflow-handling, extended build

--- fw/AIRDOS03_MAVLink/src/githash.h
+++ fw/AIRDOS03_MAVLink/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "c899d9e827d6bcb9975bc992f2cd0a8922199e11,User";
+String githash = "835de4e3890764f9e30fcd22d511afa3269412c1,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
