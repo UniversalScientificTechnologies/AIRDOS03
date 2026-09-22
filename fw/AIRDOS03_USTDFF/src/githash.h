2ce07094e71bf4032e1461b13261c426f0ab2fe0
ustbot <ust-bot@seznam.cz>
2026-09-22 13:47:21 +0000

    Merge pull request #18 from UniversalScientificTechnologies/feature/ust-format-checker-integration, extended build

--- fw/AIRDOS03_USTDFF/src/githash.h
+++ fw/AIRDOS03_USTDFF/src/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "835de4e3890764f9e30fcd22d511afa3269412c1,User";
+String githash = "0d579dadd36f15b6de2d06d1d18f02487c084f88,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
